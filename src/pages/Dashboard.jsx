8675600244570ad174075a67208367a29356819f
MikaelPemba7 <109562254+Kaelp7@users.noreply.github.com>
2024-12-13 03:51:07 +0800

    tes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -58,6 +58,7 @@ const Dashboard = () => {
 
 
     <h1 className="text-lg font-bold text-gray-800 mb-4">Peta Kelurahan Matani Tiga</h1>
+    <h1 className="text-m text-gray-800 mb-4">Menampilkan Peta Kelurahan Matani Tiga</h1>
       <br></br>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7977.532210580124!2d124.83891305!3d1.31589995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32876c9f35701853%3A0x715081e67ae9d341!2sMatani%20Tiga%2C%20Kec.%20Tomohon%20Tengah%2C%20Kota%20Tomohon%2C%20Sulawesi%20Utara!5e0!3m2!1sid!2sid!4v1733987784073!5m2!1sid!2sid" 
